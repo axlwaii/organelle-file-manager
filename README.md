f3a812a5cc98a7bdf6417fe70669f717a63dcaa5
axlwaii <markus.waitl@ecosia.org>
2022-04-16 21:24:20 +0200

    Initial commit: Version 1.0

---
+++ README.md
@@ -0,0 +1,60 @@
+# Organelle File Manager
+
+A simple file manager to directly browse, copy/paste or delete files on the Critter & Guitari Organelle.
+
+## Install
+
+1. Create the folder inside `System` on the Organelles SD card with the name `File manager`.
+2. Copy the files `main.py` and `og.py` into the `File manager` folder.
+3. In the Organelle menu go to `System` -> `File manager` to start.
+
+## Actions
+
+To open the available options press the main knob on the file you would like to perfom an action.
+
+### Browse
+
+Turning the main knob let you select files or folders. Pressing the knob on a folder will enter the folder.
+Selecting `../` will bring you to the previous folder.
+
+### Delete
+
+**Caution**: Files are deleted permanentely and can not be restored. Only files can be deleted.
+
+1. Browse to the file you would like to delete.
+2. Press the main knob and select `Delete`.
+3. Confirm the file to be permanentely deleted.
+
+### Copy & Paste
+
+**Copy**
+
+1. Browse to the file you would like to copy.
+2. Press the main knob and select `Copy**.
+3. The file has been copied and you'll see the folder structure again
+
+**Paste file to folder**
+
+1. Each folder will now have a `/` menu entry which represents the folders root.
+2. Choose `/` with the main knob and then select paste
+3. Confirm the file to be copied to the selected folder.
+
+**Overwrite an existing file**
+
+1. Browse to the file you would like to replace.
+2. Press the main know and select `Paste`.
+3. Confirm the copied file to overwrite the selected file
+
+## Version History:
+
+1.0 – Initial Release. April 17 2022
+
+## License
+
+The MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ main.py
@@ -0,0 +1,191 @@
+import os
+import subprocess
+import imp
+import sys
+import time
+import threading
+
+import og
+
+# usb or sd card
+user_dir = os.getenv("USER_DIR", "/usbdrive")
+
+# State
+file_manager_directory = user_dir
+file_manager_selected_path = ''
+file_manager_selected_file = ''
+file_manager_copied_path = ''
+
+# Helpers
+def isfile(file_name):
+  full_file_path = "/".join((file_manager_directory, file_name))
+  return os.path.isfile(full_file_path)
+
+def isdir(dir_name):
+  full_dir_path = "/".join((file_manager_directory, dir_name))
+  return os.path.isdir(full_dir_path)
+
+# UI elements
+menu = og.Menu()
+menu.items = []
+
+# lock for updating menu
+menu_lock = threading.Lock()
+
+def update_menu():
+  menu_lock.acquire()
+  try :
+    pass
+  finally :
+    menu_lock.release()
+
+# bg connection checker
+def check_status():
+  while True:
+    time.sleep(1)
+    update_menu()
+    og.redraw_flag = True
+
+# Menu actions
+def clear_menu():
+  global menu
+  og.clear_screen()
+  menu.subheader = ''
+  menu.separator = False
+  menu.items = []
+  menu.selection = 0
+  menu.cursor_offset = 0
+  menu.menu_offset = 0
+
+def folder_up():
+  global file_manager_directory
+  new_path = os.path.dirname(file_manager_directory)
+  file_manager_directory = new_path
+  draw_manager_menu()
+
+def select_directory():
+  global file_manager_directory
+  global menu
+  selected_folder = menu.items[menu.selection][2]
+  new_path = "/".join((file_manager_directory, selected_folder))
+  file_manager_directory = new_path
+  draw_manager_menu()
+
+# Action status views
+def delete_file():
+  global file_manager_selected_path
+  og.clear_screen()
+  og.println(0, "Deleted")
+  og.println(1, "---------------------")
+  og.println(2, file_manager_selected_path[-20:])
+  os.system('rm -f ' + file_manager_selected_path.replace(' ', '\ '))
+  time.sleep(2.5)
+  draw_manager_menu()
+
+def copy_file():
+  global file_manager_selected_path
+  global file_manager_copied_path
+  og.clear_screen()
+  og.println(0,"COPIED")
+  og.println(1, "---------------------")
+  og.println(2, file_manager_selected_path[-20:])
+  file_manager_copied_path = file_manager_selected_path
+  time.sleep(4)
+  draw_manager_menu()
+
+def paste_file():
+  global file_manager_copied_path
+  og.clear_screen()
+  og.println(0,"PASTED")
+  og.println(1, "---------------------")
+  og.println(2, file_manager_copied_path[-20:])
+  og.println(3, "TO " + file_manager_selected_path[-17:])
+  os.system('cp  ' + file_manager_copied_path.replace(' ', '\ ') + ' ' +  file_manager_selected_path.replace(' ', '\ '))
+  time.sleep(4)
+  file_manager_copied_path = ''
+  draw_manager_menu()
+
+# Menu views
+def confirm_delete_menu():
+  global file_manager_directory
+  global file_manager_selected_file
+  global menu
+  clear_menu()
+  menu.separator = True
+  menu.header= "DELETE " + file_manager_selected_file
+  menu.items.append(["No", draw_manager_menu])
+  menu.items.append(["Yes", delete_file])
+  menu.perform()
+
+def confirm_paste_menu():
+  global file_manager_directory
+  global file_manager_copied_path
+  global menu
+  clear_menu()
+  menu.separator = True
+  menu.header= "COPY " + file_manager_copied_path[-15:]
+  menu.subheader= "TO " + file_manager_selected_path[-17:]
+  menu.items.append(['No', draw_manager_menu])
+  menu.items.append(['Yes', paste_file])
+  menu.perform()
+
+def draw_action_menu():
+  global menu
+  global file_manager_selected_path
+  global file_manager_selected_file
+  file_manager_selected_file = menu.items[menu.selection][0]
+  if (file_manager_selected_file != '/'):
+    file_path = "/".join((file_manager_directory, file_manager_selected_file))
+  else:
+    file_path = file_manager_directory + '/'
+  file_manager_selected_path = file_path
+  clear_menu()
+  menu.header = 'ACTIONS'
+  menu.separator = True
+  if (file_manager_selected_file != '/'):
+    menu.items.append(['Copy', copy_file])
+  if (file_manager_copied_path != ''):
+    menu.items.append(['Paste', confirm_paste_menu])
+  if (file_manager_selected_file != '/'):
+    menu.items.append(['Delete', confirm_delete_menu])
+  menu.items.append(['Abort', draw_manager_menu])
+  menu.items.append([''])
+  menu.perform()
+
+def draw_manager_menu():
+  global menu
+  global file_manager_directory
+  clear_menu()
+  menu.header= 'DIR:.' + file_manager_directory[-14:]
+  files_list = sorted(filter(isfile, os.listdir(file_manager_directory)))
+  dirs_list = sorted(filter(isdir, os.listdir(file_manager_directory)))
+  if (file_manager_directory != user_dir):
+    menu.items.append(['../', folder_up])
+  if (file_manager_copied_path != ''):
+    menu.items.append(['/', draw_action_menu])
+  for dir_name in dirs_list:
+    menu.items.append(["/" + dir_name, select_directory, dir_name])
+  for file_name in files_list:
+    menu.items.append([file_name, draw_action_menu])
+  menu.items.append(['<-- Quit', quit])
+  menu.perform()
+
+# App
+def quit():
+  og.end_app()
+
+def start():
+  og.start_app()
+  draw_manager_menu()
+
+menu_updater = threading.Thread(target=check_status)
+menu_updater.daemon = True # stop the thread when we exit
+
+og.redraw_flag = True
+
+# start thread to update connection status
+menu_updater.start()
+
+# Init
+start()
+
